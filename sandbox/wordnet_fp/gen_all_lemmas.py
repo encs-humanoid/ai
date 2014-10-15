043f257af6f6ccdb3db60fa41b0ab44eda9b1535
Mark Whelan <m@townunderground.net>
2014-10-15 10:15:30 -0400

    make method calls explicit in gen_all_lemmas.py

--- sandbox/wordnet_fp/gen_all_lemmas.py
+++ sandbox/wordnet_fp/gen_all_lemmas.py
@@ -12,11 +12,10 @@ from nltk.corpus import wordnet as wn
 lemmas = set()
 for lemma_name in wn.all_lemma_names():
 	for synset in wn.synsets(lemma_name):
-		for lemma in synset.lemmas:
-			if lemma.name.find('.') < 0: # exlude lemmas with a dot in the name, because WN cannot parse them
-				lemmas.add(synset.name + '.' + lemma.name)
+		for lemma in synset.lemmas():
+			if lemma.name().find('.') < 0: # exlude lemmas with a dot in the name, because WN cannot parse them
+				lemmas.add(synset.name() + '.' + lemma.name())
 
 with open('all_lemmas.txt', 'w') as f:
 	for lemma in lemmas:
 		f.write(lemma + "\n")
-
