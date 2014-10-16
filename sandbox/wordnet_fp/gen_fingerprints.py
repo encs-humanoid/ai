e37c8084768da542c5ae94d71e437143cc6bf10e
Mark Whelan <m@townunderground.net>
2014-10-15 21:09:53 -0400

    more magic parens

--- sandbox/wordnet_fp/gen_fingerprints.py
+++ sandbox/wordnet_fp/gen_fingerprints.py
@@ -17,9 +17,9 @@ with open('vocab_lemmas.txt', 'r') as f:
 N = len(lemmas)
 
 def set_bit(fp, lemmas, lemma):
-	if lemma.name.find('.') < 0: # exlude lemmas with a dot in the name, because WN cannot parse them
+	if lemma.name().find('.') < 0: # exlude lemmas with a dot in the name, because WN cannot parse them
 		try:
-			fp.append(lemmas.index(lemma.synset.name + '.' + lemma.name))
+			fp.append(lemmas.index(lemma.synset().name + '.' + lemma.name))
 		except:
 			pass # ignore if lemma not found in the list for our vocab
 
@@ -30,7 +30,7 @@ with open('fingerprints.txt', 'w') as f:
 		ls = set() # set to collect unique lemmas for the fingerprint
 
 		# 1. Find lemmas in the same synset and add their indexes to the fingerprint
-		for l in lemma.synset.lemmas:
+		for l in lemma.synset().lemmas():
 			ls.add(l)
 		for l in lemma.antonyms():
 			ls.add(l)
@@ -39,52 +39,47 @@ with open('fingerprints.txt', 'w') as f:
 		for l in lemma.pertainyms():
 			ls.add(l)
 
-		for s in lemma.synset.causes():
-			for l in s.lemmas:
+		for s in lemma.synset().causes():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.entailments():
-			for l in s.lemmas:
+		for s in lemma.synset().entailments():
+			for l in s.lemmas():
 				ls.add(l)
-		for synset in lemma.synset.hyponyms():
-			for l in synset.lemmas:
+		for synset in lemma.synset().hyponyms():
+			for l in synset.lemmas():
 				ls.add(l)
 			for s in synset.hyponyms(): # traverse hyponyms down two levels
-				for l in synset.lemmas:
+				for l in synset.lemmas():
 					ls.add(l)
-		for synset in lemma.synset.instance_hyponyms():
-			for l in synset.lemmas:
+		for synset in lemma.synset().instance_hyponyms():
+			for l in synset.lemmas():
 				ls.add(l)
 			for s in synset.instance_hyponyms(): # traverse instance hyponyms down two levels
-				for l in s.lemmas:
+				for l in s.lemmas():
 					ls.add(l)
-		for s in lemma.synset.member_holonyms():
-			for l in s.lemmas:
+		for s in lemma.synset().member_holonyms():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.member_meronyms():
-			for l in s.lemmas:
+		for s in lemma.synset().member_meronyms():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.part_holonyms():
-			for l in s.lemmas:
+		for s in lemma.synset().part_holonyms():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.part_meronyms():
-			for l in s.lemmas:
+		for s in lemma.synset().part_meronyms():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.similar_tos():
-			for l in s.lemmas:
+		for s in lemma.synset().similar_tos():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.substance_holonyms():
-			for l in s.lemmas:
+		for s in lemma.synset().substance_holonyms():
+			for l in s.lemmas():
 				ls.add(l)
-		for s in lemma.synset.substance_meronyms():
-			for l in s.lemmas:
+		for s in lemma.synset().substance_meronyms():
+			for l in s.lemmas():
 				ls.add(l)
 
 		for l in ls:
 			set_bit(fp, lemmas, l)
 				
 		f.write(lemmas[i] + ":" + str(fp) + "\n")
-
-
-
-
-
