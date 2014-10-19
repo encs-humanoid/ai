8c707461e57a7d33e80c763c201c4b1bd8b3db9e
Mark Whelan <m@townunderground.net>
2014-10-19 00:27:43 -0400

    update rebuild script

--- sandbox/wordnet_fp/rebuild.sh
+++ sandbox/wordnet_fp/rebuild.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-python gen_vocab_lemmas.py
-python check_vocab.py
-python gen_fingerprints.py
-python opt_fingerprints.py
-head vocab_lemmas.txt > train_lemmas.txt
+python gen_vocab_lemmas.py melville-moby_dick.txt
+# python check_vocab.py
+python gen_fingerprints2.py
+# python opt_fingerprints.py
+head -1000 vocab_lemmas.txt > train_lemmas.txt
 date >> runs.out
 time unbuffer python train_nupic.py >> runs.out
+python test_nupic.py
