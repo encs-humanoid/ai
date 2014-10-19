b8c68cc2ad89f5dc896cb22f72bee2e2ce2be9d6
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2014-10-18 23:25:10 -0400

    added identification of duplicate SDRs from lemma to sdr mapping on random spatial pooler

--- sandbox/wordnet_fp/test_nupic.py
+++ sandbox/wordnet_fp/test_nupic.py
@@ -14,4 +14,13 @@ if __name__ == "__main__":
 	with open("spatial_pooler.p", "r") as f:
 	    trainer = pickle.load(f)
 
-	print trainer
+	lemma_sdrs = trainer.lemma_to_sdr.values()
+
+	sdr_to_lemma = dict()
+
+	for lemma_sdr in lemma_sdrs:
+		if sdr_to_lemma.has_key(lemma_sdr.last_sdr_key) and lemma_sdr.fp != sdr_to_lemma[lemma_sdr.last_sdr_key].fp:
+			print "Duplicate SDR:", lemma_sdr.lemma, "/", sdr_to_lemma[lemma_sdr.last_sdr_key].lemma
+		else:
+			sdr_to_lemma[lemma_sdr.last_sdr_key] = lemma_sdr
+
