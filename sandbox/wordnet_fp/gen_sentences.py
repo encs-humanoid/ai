f7d323114fa0d9aecd5a856ffd3087f4506278ba
Mark Whelan <m@townunderground.net>
2014-10-23 16:20:28 -0400

    exit 1 if failure

--- sandbox/wordnet_fp/gen_sentences.py
+++ sandbox/wordnet_fp/gen_sentences.py
@@ -43,7 +43,7 @@ def generate_fingerprints(generator, sent):
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print "Usage: " + sys.argv[0] + " <filename>"
-		sys.exit(0)
+		sys.exit(1)
 
 	input_file = sys.argv[1]
 
