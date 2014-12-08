21eb5d53c0a752c4ec5123d7e2d90168555fbff7
Mark Whelan <m@townunderground.net>
2014-12-08 17:19:30 -0500

    pair edits

--- sandbox/chatbot_ros_node/pairs.py
+++ sandbox/chatbot_ros_node/pairs.py
@@ -11,11 +11,6 @@ pairs = (
     "Hi... how are you doing?",
     "Hello, do I know you?")),
 
-  (r'(.*)',
-  ( "My hearing is weak. What I think you said was %1",
-    "Why do you say that?",
-    "Gosh! I would never have thought of that!")),
-
   (r'You are (.*)',
   ( "I know you are %1, but what am I?",
     "What makes you say I am %1?")),
@@ -24,6 +19,15 @@ pairs = (
   ( "I am so pleased that you are pleased (.*)")),
 
   (r'robot',
-    "Yes, let's talk about robots. I like robots. What about you?",
+  ( "Yes, let's talk about robots. I like robots. What about you?",
     "Ahh, robots are one of my favorite topics."))
+
+  (r'I am (.*)',
+  ( "Hello %1. I am glad to meet you!",
+    "Are you sure you are %1?")),
+
+  (r'(.*)',
+  ( "My hearing is weak. What I think you said was %1",
+    "Why do you say that?",
+    "Gosh! I would never have thought of that!")),
 )
