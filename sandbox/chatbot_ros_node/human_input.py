6692e235119a1d2101fd57f3dfb53d6cd6d820c5
Ubuntu <ubuntu@ip-172-31-18-224.us-west-2.compute.internal>
2016-02-20 16:29:58 +0000

    added display of response

--- sandbox/chatbot_ros_node/human_input.py
+++ sandbox/chatbot_ros_node/human_input.py
@@ -11,15 +11,73 @@
 import rospy
 from std_msgs.msg import String
 
+
+
+# The following from http://blog.mathieu-leplatre.info/colored-output-in-console-with-python.html
+#===================================================================
+import sys
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#following from Python cookbook, #475186
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+has_colours = has_colours(sys.stdout)
+
+
+def printout(text, colour=WHITE):
+    if has_colours:
+	seq = "\x1b[1;%dm" % (30+colour) + text + "\x1b[0m"
+	sys.stdout.write(seq)
+    else:
+	sys.stdout.write(text)
+#===================================================================
+
+
+LINE_LEN = 80
+wait_for_response = False
+
+
+def on_say(msg):
+    global wait_for_response
+    text = msg.data
+    if len(text) > LINE_LEN:
+	printout(text + '\n', GREEN)
+    else:
+    	printout(' ' * (LINE_LEN - len(text)) + text + ' <\n', GREEN)
+    print
+    wait_for_response = False
+
+
 def talker():
+    global wait_for_response
     pub = rospy.Publisher('recognized_speech', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
+    sub = rospy.Subscriber('/say', String, on_say)
+    rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
         str = raw_input("> ")
-        rospy.loginfo(str)
+        #rospy.loginfo(str)
+	wait_for_response = True
         pub.publish(str)
+	while wait_for_response and not rospy.is_shutdown():
+	    rate.sleep()
+
 
 if __name__ == "__main__":
     try:
         talker()
-    except rospy.ROSInterruptException: pass
+    except rospy.ROSInterruptException:
+	pass
+
+
