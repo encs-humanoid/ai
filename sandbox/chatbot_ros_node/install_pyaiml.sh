836938890d6c105026aef8d67fe9e3405fdaf417
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2023-04-30 08:02:36 -0400

    uncommented git clone

--- sandbox/chatbot_ros_node/install_pyaiml.sh
+++ sandbox/chatbot_ros_node/install_pyaiml.sh
@@ -5,7 +5,7 @@ PYAIML_DIR=$HOME/catkin_ws/src/ai/sandbox/chatbot_ros_node/pyaiml
 # TODO Figure out where the latest repository for pyaiml is.
 # Until then, copy the distribution from another computer, then run this script to install it.
 #git clone git://pyaiml.git.sourceforge.net/gitroot/pyaiml/pyaiml $PYAIML_DIR
-#git clone https://github.com/creatorrr/pyAIML.git $PYAIML_DIR
+git clone https://github.com/creatorrr/pyAIML.git $PYAIML_DIR
 CUR_DIR=`pwd`
 cd $PYAIML_DIR
 sudo python setup.py install
